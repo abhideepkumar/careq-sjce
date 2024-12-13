7465bb72a40dbf1d9fd98d61b8c11c14e049005a
Abhideep <itsabhideep@gmail.com>
2024-12-13 15:40:22 +0530

    update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,20 +11,6 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
-<<<<<<< HEAD
-// import userRouter from "./routes/user.routes.js";
-// import productRoute from "./routes/product.routes.js"
-// import postRoute from "./routes/post.routes.js"
-
-// // routes decclaration
-// app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/products", productRoute);
-// app.use("/api/v1/post", postRoute);
-
-//****************************************************8 */
-// //routes import
-
-=======
 //routes import
 import bedRouter from "./routes/bedRoutes.js";
 import inventoryRouter from "./routes/inventoryRoutes.js";
@@ -36,6 +22,5 @@ app.use("/api/v1/beds", bedRouter);
 app.use("/api/v1/inventory", inventoryRouter);
 app.use('/api/v1/departments', departmentRoutes);
 app.use('/api/v1/doctors', doctorRoutes);
->>>>>>> 29dc20c9baf67099d025835ef910b0611e5855cb
 
 export { app };
