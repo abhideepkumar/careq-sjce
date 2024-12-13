74922a695d37e5ee6191e1231f4daef36aa4a03f
SunidhiH <vaibhavghugretkar23@gmail.com>
2024-12-13 16:26:07 +0530

    added routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,10 +17,15 @@ import inventoryRouter from "./routes/inventoryRoutes.js";
 import departmentRoutes from './routes/department.routes.js';
 import doctorRoutes from './routes/doctor.routes.js';
 
-// routes decclaration
+// routes declaration
 app.use("/api/v1/beds", bedRouter);
 app.use("/api/v1/inventory", inventoryRouter);
 app.use('/api/v1/departments', departmentRoutes);
 app.use('/api/v1/doctors', doctorRoutes);
+app.use ('/api/v1/')
 
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/hospitals', hospitalRoutes);
+app.use('/api/v1/appointments', appointmentRoutes);
+app.use('/api/v1/queues', queueRoutes);
 export { app };
